45c84a4d9569012722d2253fbd8cf88aa6486a50
Erdal ALTIN <erdalaltin@gmail.com>
2026-03-19 16:56:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -611,7 +611,6 @@ if [ -x "$(command -v docker)" ] && [ "$(docker compose version)" ]; then
 			echo "completed setup"
 			echo
 			echo "Website: https://$domain_name"
-			echo "Requirements: https://$domain_name/requirements.php"
 			echo "Portainer: https://$domain_name:9001"
 			echo "phpMyAdmin: https://$domain_name:9090"
 			echo
